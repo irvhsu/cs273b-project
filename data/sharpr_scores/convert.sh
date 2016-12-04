2833e0b6f64eafc17626ee4ae131234d623c0737
Joe Paggi <jpaggi@stanford.edu>
2016-12-04 11:59:43 -0800

    Framework for interpretation

---
+++ data/sharpr_scores/convert.sh
@@ -0,0 +1,6 @@
+./bigWigToWig bigWig/browser_SV40P_HEPG2.bw wig/SV40P_HEPG2.wig
+./bigWigToWig bigWig/browser_SV40P_K562.bw wig/SV40P_K562.wig
+./bigWigToWig bigWig/browser_minP_HEPG2.bw wig/minP_HEPG2.wig
+./bigWigToWig bigWig/browser_minP_K562.bw wig/minP_K562.wig
+./bigWigToWig bigWig/browser_combinedP_K562.bw wig/combinedP_K562.wig
+./bigWigToWig bigWig/browser_combinedP_HEPG2.bw wig/combinedP_HEPG2.wig
